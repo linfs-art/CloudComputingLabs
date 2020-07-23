20e905da2571087a6f5246c9ad20dd2f1e990b09
linfs-art <linfs_08@163.com>
2020-07-23 16:20:35 +0800

    support basic/advanced mode

--- Lab1/sudoku_main.cc
+++ Lab1/sudoku_main.cc
@@ -11,13 +11,17 @@
 using namespace std;
 
 /* switch solving algorithm, option:BASIC/DANCE/MINA/MINAC */
-_FUNC_TYPE DEFAULT_FUNC_TYPE = DANCE;       
+_FUNC_TYPE DEFAULT_FUNC_TYPE = DANCE;      
+
+
+/* options:BASIC_MODE/ADVANCED_MODE */     
+_MODE_TYPE DEFAULT_MODE_TYPE = BASIC_MODE;  
 
 
 int main ()
 {
 	//Cut the cackle and come to the horses. Let's go!!!
-	start_thrd (DEFAULT_FUNC_TYPE);
+	start_thrd (DEFAULT_FUNC_TYPE, DEFAULT_MODE_TYPE);
 	sudoku_thrd_join ();
 
 	/* BASIC_MODE only */
